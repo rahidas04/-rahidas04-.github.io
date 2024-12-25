083b374391df14c5614a487c922f788d96ebb04d
rahidas04 <56899753+rahidas04@users.noreply.github.com>
2024-12-25 02:48:50 +0000

    Entire Website Commit - Rahi Dasgupta

---
+++ scripts/homepageinteractivity.js
@@ -0,0 +1,9 @@
+const typed = new Typed('#typed', {
+    strings: ["Hi, I'm Rahi!", "I'm a Data Enthusiast.", "Kansas City Chiefs Fan.", "& Menstrual Equality Advocate", "Explore my website to learn more!"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1500,
+    loop: true,
+    showCursor: false,
+  });
+  
---
+++ scripts/resumebutton.js
@@ -0,0 +1,4 @@
+document.getElementById("resume").addEventListener("click", function () {
+  // Replace 'resume.pdf' with the correct path to your resume file
+  window.open("Resume - Rahi Dasgupta.pdf", "_blank");
+});
